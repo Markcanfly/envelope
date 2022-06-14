3eb4cb70ca277a77a72a6adbc12f9a994d05ebb8
Márk Varga <varga.mrk@gmail.com>
2022-06-14 22:48:08 +0200

    Add new message tests

--- middleware/message_test.go
+++ middleware/message_test.go
@@ -2,10 +2,26 @@ package middleware
 
 import (
 	"bytes"
+	"encoding/json"
+	"envelope/models"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
+
+func helpCreateMessage(content string, timestamp int64) *httptest.ResponseRecorder {
+	message := fmt.Sprintf(`{"content":"%s", "unlocks_at": %d}`, content, timestamp)
+	req, _ := http.NewRequest("POST", "", bytes.NewBufferString(message))	
+	req.Header.Set("Authorization", token)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(CreateMessage)
+
+	handler.ServeHTTP(rr, req)
+	return rr
+}
 
 func TestGetOpenedMessagesEmpty(t *testing.T) {
 	req, err := http.NewRequest("GET", "", nil)	
@@ -44,4 +60,36 @@ func TestTryCreateMessageWithoutAuth(t *testing.T) {
 	}
 }
 
-// TODO add message creation and viewing tests
+func TestCreateMessage(t *testing.T) {
+	message := fmt.Sprintf(`{"content":"titkos üzenet", "unlocks_at": %d}`, time.Now().Unix())
+	req, err := http.NewRequest("POST", "", bytes.NewBufferString(message))	
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", token)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(CreateMessage)
+
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusCreated {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			rr.Code, http.StatusCreated)
+	}
+}
+
+func TestCreateMessageCheckData(t *testing.T) {
+	currenttime := time.Now().Unix()
+	rr := helpCreateMessage("titkos üzenet", currenttime)
+	var message models.Message
+	json.Unmarshal(rr.Body.Bytes(), &message)
+	if message.Content != "titkos üzenet" {
+		t.Errorf("handler returned wrong content: got %v want %v",
+			message.Content, "titkos üzenet")
+	}
+	if message.UnlocksAt != currenttime {
+		t.Errorf("handler returned wrong unlocks_at: got %v want %v",
+			message.UnlocksAt, currenttime)
+	}
+}
